d1ce1545e514518fea99255899ca96df67224bed
Rushikeshbhavsar20 <56561849+Rushikeshbhavsar20@users.noreply.github.com>
2024-09-23 22:15:39 +0530

    understanding jsx , React elements and components and  functional components way of writing functional components

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-
-
-const heading = React.createElement('h1',{id:"header"},"Hello from React application")
-
-
+import ReactDOM from "react-dom/client";
 
 /*
 creating nested elemrnt 
@@ -19,7 +13,7 @@ creating nested elemrnt
     React element are object and gets converted to html which browser understands 
 */
 
-
+/*
 const parent  = React.createElement("div",{id:'parent'},[
   React.createElement("div",{id:"child"},[
     React.createElement("h1",{id:'name1'},"Hello from h1 tag"),
@@ -41,3 +35,75 @@ const ok = React.createElement("div",{id:"box"},[
 ])
 
 root.render(ok) 
+*/
+
+//creating react in newer way
+
+
+//creating a react element 
+const jsxheading = ( <h1 id="ok" >Namaste react this is 
+           rushikesh bhavsar
+   </h1> 
+   );
+
+//creating functional component
+const Component = () => {
+   return <h1> Welcome to react </h1>
+}
+
+
+//creating functional Component-compostion
+
+const Component2 = () => {
+  return <div className="composition">
+           <Component/>
+           <h1> this is react </h1> 
+    </div>
+  
+}
+
+//types of defining various functional components 
+
+//with () brackets we dont have to write return
+const Component3 = () => (
+ <h1> Welcome to react </h1>
+);
+
+
+
+//with {} we have to define return compulsory
+const Component4 = () => {
+ return  <div className="composition">
+          <Component/>
+          <h1> this is react </h1> 
+   </div>
+ 
+};
+
+//we can write without  any {} and return statement if return on the single line
+const Componet = () =>  <h1> this is react </h1> ;
+
+
+
+const Component5 = () => (
+  <div className="composition">
+          <Component/>
+          <h1> this is react </h1> 
+   </div>
+ 
+);
+
+
+
+
+
+
+
+
+
+
+
+console.log(Componet)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Componet/>);
